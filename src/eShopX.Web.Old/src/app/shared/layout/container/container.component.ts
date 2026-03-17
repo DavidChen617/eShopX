32a64083a6dc8d097c2734995213303fe5556de8
DavidChen617 <a880617c@gmail.com>
2026-03-17 19:04:52 +0800

    chore: remove eShopX.Web.Old project

--- src/eShopX.Web.Old/src/app/shared/layout/container/container.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ui-container',
-  standalone: true,
-  templateUrl: './container.component.html',
-})
-export class ContainerComponent {}
--- src/eShopX.Web.Old/src/environments/environment.prod.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-  apiBaseUrl: '__API_BASE_URL__',
-};
